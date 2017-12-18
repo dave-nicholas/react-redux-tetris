9b0703967bc09956108d6ff16a5d17ea2d3493ef
Dave Nicholas <davenicholasuk@gmail.com>
2017-12-18 21:15:46 +0000

    Add snapshot test

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -3,6 +3,8 @@ import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-
-  shallow(<App classes={{ root: '', navButton: '' }} push={location => {}} />);
+  const app = shallow(
+    <App classes={{ root: '', navButton: '' }} push={location => {}} />
+  );
+  expect(app).toMatchSnapshot();
 });
